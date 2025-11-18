1ad8dcf5f63e8dc8ed10e160b28d9e9b35bc30aa
Apavozkov <apavozkov@bk.ru>
2025-11-18 21:07:23 +0300

    Удалены части, связанные с прошлой версией конфигурации

--- main.go
+++ main.go
@@ -1,18 +1,1 @@
 package main
-
-import (
-	"durland/config"
-	"durland/models"
-	"log"
-)
-
-func main() {
-	if err := config.LoadConfig("config.json"); err != nil {
-		log.Fatal(err)
-	}
-
-	cfg := config.GetConfig()
-
-	durlian := models.NewDurlian(cfg.Races, cfg.Locations)
-	log.Printf("Создан чел: %+v", durlian.KnownInfo)
-}
